3f1625f8765f7a0de63599399422ce31ab97e531
Shubham Sharma <shubham.sharma@hyperdart.com>
2024-07-28 22:26:35 +0530

    service worker added

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Button } from '@mui/material';
 
 function Test() {
+
+  const [name, setName] = React.useState("")
+
+  const pushUser = async() => {
+    const url = "https://my-json-server.typicode.com/Shubhamsharma585/SmartSteps/profile";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    setName(json.name);
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+
+  }
+
   return (
-    <div>Test</div>
+    <>
+    <h1>Name is {name}</h1>
+    <Button onClick={() => pushUser()} variant='contained'>Click</Button>
+    </>
   )
 }
 
